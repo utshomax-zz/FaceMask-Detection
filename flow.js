130942c1d1bf394b50570f798caaad38fff90505
UTSAB UTSHO <utsab8008@gmail.com>
2021-05-07 09:56:20 +0530

    Update flow.js

--- flow.js
+++ flow.js
@@ -8,26 +8,20 @@ let notifier = document.getElementById("notify")
 let loader = document.getElementById("loader")
 var sndtrue = new Audio('./audio/sd_c.mp3')
 var sndfalse = new Audio('./audio/sd_w.mp3')
-// Load the image model and setup the webcam
+
 async function init() {
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
 
-    // load the model and metadata
-    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-    // or files from your local hard drive
-    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
     model = await tmImage.load(modelURL, metadataURL);
     maxPredictions = model.getTotalClasses();
 
-    // Convenience function to setup a webcam
-    const flip = true; // whether to flip the webcam
-    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-    await webcam.setup(); // request access to the webcam
+    const flip = true;
+    webcam = new tmImage.Webcam(200, 200, flip); 
+    await webcam.setup();
     await webcam.play();
     window.requestAnimationFrame(loop);
 
-    // append elements to the DOM
     loader.classList.add('hidden')
     document.getElementById("webcam-container").appendChild(webcam.canvas);
     
@@ -41,7 +35,7 @@ async function beep(cmd) {
     }
 }
 async function loop() {
-    webcam.update(); // update the webcam frame
+    webcam.update(); 
     await predict();
     window.requestAnimationFrame(loop);
 }
@@ -66,9 +60,8 @@ function badge_fail(){
         Mask is Mandatory</div>
     </div>`
 }
-// run the webcam image through the image model
+
 async function predict() {
-    // predict can take in an image, video or canvas html element
     const prediction = await model.predict(webcam.canvas);
     for (let i = 0; i < maxPredictions; i++) {
         if(prediction[i].probability > .90){
