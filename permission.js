669def2c2b4dbccf1c1adcad215bddf6bf15ecfc
UTSAB UTSHO <utsab8008@gmail.com>
2021-05-07 09:55:51 +0530

    v 0.0.1

--- permission.js
+++
@@ -1,8 +0,0 @@
-async function getMedia() {
-    try {
-      await navigator.mediaDevices.getUserMedia({audio: false, video: true});
-    } catch(err) {
-      alert("Camera Permission is Required!")
-    }
-  }
-getMedia()
