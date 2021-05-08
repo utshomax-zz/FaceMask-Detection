08945d571edc4eb8a4b40ea9b91bdea59c986216
UTSAB UTSHO <utsab8008@gmail.com>
2021-05-09 01:12:32 +0530

    vue

---
+++ client/facemask/src/App.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="flex-col">
+    <div class="headerText">FACE MASK DETECTION</div>
+    <loader v-if="loading"></loader>
+    <div class="cam-container flex-col">
+      <div id="cameraPreview" class="cameraPreview flex-center">
+      </div>
+      <notify :state="maskS"></notify>
+    </div>
+  </div>
+</template>
+
+<script>
+  /* eslint-disable no-unused-vars */
+  import * as tf from '@tensorflow/tfjs';
+  import * as tmImage from '@teachablemachine/image';
+  import {
+    Plugins
+  } from "@capacitor/core"
+  const {
+    CameraPreview
+  } = Plugins;
+  //import '@capacitor-community/camera-preview'
+  import loader from './components/Loader'
+  import notify from './components/Notify'
+  const URL = "https://teachablemachine.withgoogle.com/models/LUWkaHPj4/"
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+  const DTCURL = "https://dtcmask.herokuapp.com/api/dtcdata"
+
+  var isMasked = false;
+  var isNotMasked = false;
+  let model, maxPredictions;
+  const cameraSampleOptions = {
+    quality: 40,
+    height: 100,
+    width: 100,
+    position: 'front',
+    parent: 'cameraPreview',
+    className: 'cameraPriview',
+  }
+  var sndtrue = new Audio(require("./assets/audio/sd_c.mp3"))
+  var sndfalse = new Audio(require("./assets/audio/sd_w.mp3"))
+  export default {
+    name: 'App',
+    components: {
+      // eslint-disable-next-line vue/no-unused-components
+      loader,
+      notify
+    },
+    data() {
+      return {
+        ready: false,
+        maskS: 0,
+        loading: true
+      }
+    },
+    async mounted() {
+      model = await tmImage.load(modelURL, metadataURL);
+      maxPredictions = model.getTotalClasses();
+      console.log(maxPredictions)
+      this.ready = true
+      CameraPreview.start(cameraSampleOptions)
+      this.init()
+      this.loading = false
+    },
+    methods: {
+      init() {
+        const self = this
+        setInterval(function () {
+            self.predict()
+          },
+          800);
+      },
+      async predict() {
+        const result = await CameraPreview.capture(cameraSampleOptions);
+        let base64PictureData = result.value;
+        let canvas = await this.getCanvas(base64PictureData)
+        const prediction = await model.predict(canvas);
+        for (let i = 0; i < maxPredictions; i++) {
+          if (prediction[i].probability > .90) {
+            if (prediction[i].className == 'MASK' && !isMasked) {
+              this.badge_ok()
+              isMasked = true
+              isNotMasked = false
+              console.log('ok')
+            }
+            if (prediction[i].className == 'NO MASK' && !isNotMasked) {
+              this.badge_fail()
+              isMasked = false
+              isNotMasked = true
+              console.log('nok')
+            }
+          }
+
+        }
+      },
+      async badge_fail() {
+        this.beep(false)
+        this.maskS = 2
+        this.sendToAdmin(false)
+      },
+      async badge_ok() {
+          this.beep(true)
+          this.maskS = 1
+          this.sendToAdmin(true)
+        }
+
+        ,
+      async beep(cmd) {
+        if (cmd) {
+          sndtrue.play()
+        } else {
+          sndfalse.play()
+        }
+      },
+      async getCanvas(base) {
+        return new Promise((resolve, reject) => {
+          try {
+            let image = new Image();
+            image.src = `data:image/png;base64,${base}`;
+            resolve(image);
+          } catch (error) {
+            reject(error);
+          }
+        })
+      },
+      async sendToAdmin(maskState) {
+        var data = {
+          state: maskState
+        }
+        const response = await fetch(DTCURL, {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+        console.log(await response.json());
+      }
+    },
+  }
+</script>
+
+<style>
+  .cameraPreview {
+    display: flex;
+    height: 100%;
+    width: 90%;
+    overflow: hidden;
+    text-align: center;
+    padding: 10px;
+    border-radius: 10px;
+  }
+</style>
---
+++ client/facemask/src/components/Notify.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="notify" class="flex-col">
+        <h5 v-if="state==0">
+            Loading ...
+        </h5>
+        <div v-if="state==2"><svg xmlns="http://www.w3.org/2000/svg" class="icon c-c" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <div class="text">Opps ! <br>
+                Mask is Mandatory</div>
+        </div>
+        <div v-if="state==1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+            </svg>
+            <div class="text">Thank You ! <br>
+                For Using Mask</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            state: {
+                type: Number,
+                default: 0
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
